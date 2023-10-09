2a107d9a73bc6c19309bcf3ff3563f0d042869c7
Andrew Bustos <infernious@live.com>
2023-10-08 20:10:55 -0700

    autoplay slider

---
+++ app/Slider.jsx
@@ -0,0 +1,44 @@
+"use client"
+
+import { Swiper, SwiperSlide } from "swiper/react"
+
+// Import Swiper styles
+import "swiper/css"
+import "swiper/css/pagination"
+import "swiper/css/navigation"
+
+import { Autoplay, Pagination } from "swiper/modules"
+
+export const Slider = () => {
+	const pagination = {
+		clickable: true,
+		renderBullet: function (index, className) {
+			return '<span class="' + className + '">' + "</span>"
+		},
+	}
+
+	return (
+		<>
+			<Swiper
+				spaceBetween={30}
+				centeredSlides={true}
+				autoplay={{
+					delay: 2500,
+					disableOnInteraction: false,
+				}}
+				pagination={pagination}
+				modules={[Autoplay, Pagination]}
+			>
+				<SwiperSlide>
+					<img src='test.webp' className='h-screen' />
+				</SwiperSlide>
+				<SwiperSlide>
+					<img src='test.webp' className='h-screen' />
+				</SwiperSlide>
+				<SwiperSlide>
+					<img src='test.webp' className='h-screen' />
+				</SwiperSlide>
+			</Swiper>
+		</>
+	)
+}
--- app/page.js
+++ app/page.js
@@ -1,54 +1,10 @@
 import Image from "next/image"
+import { Slider } from "./Slider"
 
 export default function Home() {
 	return (
 		<main className='flex min-h-screen flex-col items-center'>
-			<div className='carousel w-full h-screen'>
-				<div id='slide1' className='carousel-item relative w-full'>
-					<img src='test.webp' className='w-full object-cover	' />
-					<div className='absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2'>
-						<a href='#slide4' className='btn btn-accent btn-circle'>
-							❮
-						</a>
-						<a href='#slide2' className='btn btn-accent btn-circle'>
-							❯
-						</a>
-					</div>
-				</div>
-				<div id='slide2' className='carousel-item relative w-full'>
-					<img src='test.webp' className='w-full object-cover	' />
-					<div className='absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2'>
-						<a href='#slide1' className='btn btn-accent btn-circle'>
-							❮
-						</a>
-						<a href='#slide3' className='btn btn-accent btn-circle'>
-							❯
-						</a>
-					</div>
-				</div>
-				<div id='slide3' className='carousel-item relative w-full'>
-					<img src='test.webp' className='w-full object-cover	' />
-					<div className='absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2'>
-						<a href='#slide2' className='btn btn-accent btn-circle'>
-							❮
-						</a>
-						<a href='#slide4' className='btn btn-accent btn-circle'>
-							❯
-						</a>
-					</div>
-				</div>
-				<div id='slide4' className='carousel-item relative w-full'>
-					<img src='test.webp' className='w-full object-cover	' />
-					<div className='absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2'>
-						<a href='#slide3' className='btn btn-accent btn-circle'>
-							❮
-						</a>
-						<a href='#slide1' className='btn btn-accent btn-circle'>
-							❯
-						</a>
-					</div>
-				</div>
-			</div>
+			<Slider />
 			<div className='hero min-h-screen'>
 				<div className='hero-content text-center'>
 					<div className='max-w-3xl'>
