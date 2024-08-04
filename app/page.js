d56a6bc92f420af4a5f81203b685bca8d2090826
Andrew Bustos <infernious@live.com>
2024-08-03 23:14:38 -0700

    add concrete image

--- app/page.js
+++ app/page.js
@@ -1,16 +1,16 @@
-import Image from "next/image"
-import { Slider } from "./Slider"
-import { Web3Script } from "./web3script.js"
+import Image from 'next/image';
+import { Slider } from './Slider';
+import { Web3Script } from './web3script.js';
 
 export default function Home() {
 	return (
-		<main className='flex min-h-screen flex-col items-center'>
+		<main className="flex min-h-screen flex-col items-center">
 			<Slider />
-			<div className='hero min-h-fit py-10'>
-				<div className='hero-content text-center'>
-					<div className='max-w-3xl'>
-						<h1 className='text-6xl font-bold'>OUR WORK</h1>
-						<p className='text-lg'>
+			<div className="hero min-h-fit py-10">
+				<div className="hero-content text-center">
+					<div className="max-w-3xl">
+						<h1 className="text-6xl font-bold">OUR WORK</h1>
+						<p className="text-lg">
 							At Herrera Builders LLC, we believe that construction is more than
 							a business; it&apos;s a calling. It&apos;s about building the
 							future, one project at a time. From residential construction to
@@ -23,138 +23,137 @@ export default function Home() {
 				</div>
 			</div>
 			<div
-				className='hero h-96 max-w-full'
+				className="hero h-96 max-w-full"
 				style={{
-					backgroundImage:
-						"url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)",
+					backgroundImage: 'url(/concrete.jpeg)',
 				}}
 			>
-				<div className='hero-overlay bg-opacity-60'></div>
-				<div className='hero-content text-center'>
-					<div className='max-w-md'>
-						<a href='#contact' className='link link-accent text-5xl font-bold'>
+				<div className="hero-overlay bg-opacity-60"></div>
+				<div className="hero-content text-center">
+					<div className="max-w-md">
+						<a href="#contact" className="link link-accent text-5xl font-bold">
 							Concrete
 						</a>
 					</div>
 				</div>
 			</div>
 			<div
-				className='hero h-96 max-w-full'
+				className="hero h-96 max-w-full"
 				style={{
-					backgroundImage: "url(/framinn.jpg)",
+					backgroundImage: 'url(/framinn.jpg)',
 				}}
 			>
-				<div className='hero-overlay bg-opacity-60'></div>
-				<div className='hero-content text-center'>
-					<div className='max-w-md'>
-						<a href='#contact' className='link link-accent text-5xl font-bold'>
+				<div className="hero-overlay bg-opacity-60"></div>
+				<div className="hero-content text-center">
+					<div className="max-w-md">
+						<a href="#contact" className="link link-accent text-5xl font-bold">
 							Framing
 						</a>
 					</div>
 				</div>
 			</div>
 			<div
-				className='hero h-96 max-w-full'
+				className="hero h-96 max-w-full"
 				style={{
-					backgroundImage: "url(/fence.jpg)",
+					backgroundImage: 'url(/fence.jpg)',
 				}}
 			>
-				<div className='hero-overlay bg-opacity-60'></div>
-				<div className='hero-content text-center'>
-					<div className='max-w-md'>
-						<a href='#contact' className='link link-accent text-5xl font-bold'>
+				<div className="hero-overlay bg-opacity-60"></div>
+				<div className="hero-content text-center">
+					<div className="max-w-md">
+						<a href="#contact" className="link link-accent text-5xl font-bold">
 							Fencing
 						</a>
 					</div>
 				</div>
 			</div>
 			<div
-				className='hero h-96 max-w-full'
+				className="hero h-96 max-w-full"
 				style={{
-					backgroundImage: "url(/demol.jpg)",
+					backgroundImage: 'url(/demol.jpg)',
 				}}
 			>
-				<div className='hero-overlay bg-opacity-60'></div>
-				<div className='hero-content text-center'>
-					<div className='max-w-md'>
-						<a href='#contact' className='link link-accent text-5xl font-bold'>
+				<div className="hero-overlay bg-opacity-60"></div>
+				<div className="hero-content text-center">
+					<div className="max-w-md">
+						<a href="#contact" className="link link-accent text-5xl font-bold">
 							Demo
 						</a>
 					</div>
 				</div>
 			</div>
 			<div
-				className='hero h-96 max-w-full'
+				className="hero h-96 max-w-full"
 				style={{
-					backgroundImage: "url(/brr2.jpg)",
-					backgroundSize: "cover",
-					backgroundRepeat: "no-repeat",
+					backgroundImage: 'url(/brr2.jpg)',
+					backgroundSize: 'cover',
+					backgroundRepeat: 'no-repeat',
 				}}
 			>
-				<div className='hero-overlay bg-opacity-60'></div>
-				<div className='hero-content text-center'>
-					<div className='max-w-md'>
-						<a href='#contact' className='link link-accent text-5xl font-bold'>
+				<div className="hero-overlay bg-opacity-60"></div>
+				<div className="hero-content text-center">
+					<div className="max-w-md">
+						<a href="#contact" className="link link-accent text-5xl font-bold">
 							Bathrooms
 						</a>
 					</div>
 				</div>
 			</div>
 			<div
-				className='hero h-96 max-w-full'
+				className="hero h-96 max-w-full"
 				style={{
-					backgroundImage: "url(/hadd.jpg)",
+					backgroundImage: 'url(/hadd.jpg)',
 				}}
 			>
-				<div className='hero-overlay bg-opacity-60'></div>
-				<div className='hero-content text-center'>
-					<div className='max-w-md'>
-						<a href='#contact' className='link link-accent text-5xl font-bold'>
+				<div className="hero-overlay bg-opacity-60"></div>
+				<div className="hero-content text-center">
+					<div className="max-w-md">
+						<a href="#contact" className="link link-accent text-5xl font-bold">
 							Home Additions
 						</a>
 					</div>
 				</div>
 			</div>
 			<div
-				className='hero h-96 max-w-full'
+				className="hero h-96 max-w-full"
 				style={{
-					backgroundImage: "url(/paint.jpg)",
-					backgroundPosition: "0% 25%",
-					backgroundRepeat: "no-repeat",
+					backgroundImage: 'url(/paint.jpg)',
+					backgroundPosition: '0% 25%',
+					backgroundRepeat: 'no-repeat',
 				}}
 			>
-				<div className='hero-overlay bg-opacity-60'></div>
-				<div className='hero-content text-center'>
-					<div className='max-w-md'>
-						<a href='#contact' className='link link-accent text-5xl font-bold'>
+				<div className="hero-overlay bg-opacity-60"></div>
+				<div className="hero-content text-center">
+					<div className="max-w-md">
+						<a href="#contact" className="link link-accent text-5xl font-bold">
 							Interior & Exterior Painting
 						</a>
 					</div>
 				</div>
 			</div>
 			<div
-				className='hero h-96 max-w-full'
+				className="hero h-96 max-w-full"
 				style={{
-					backgroundImage: "url(/drw.jpg)",
-					backgroundSize: "cover",
-					backgroundRepeat: "no-repeat",
+					backgroundImage: 'url(/drw.jpg)',
+					backgroundSize: 'cover',
+					backgroundRepeat: 'no-repeat',
 				}}
 			>
-				<div className='hero-overlay bg-opacity-60'></div>
-				<div className='hero-content text-center'>
-					<div className='max-w-md'>
-						<a href='#contact' className='link link-accent text-5xl font-bold'>
+				<div className="hero-overlay bg-opacity-60"></div>
+				<div className="hero-content text-center">
+					<div className="max-w-md">
+						<a href="#contact" className="link link-accent text-5xl font-bold">
 							Drywall
 						</a>
 					</div>
 				</div>
 			</div>
-			<div className='hero min-h-screen' id='contact'>
-				<div className='hero-content py-16 flex-col max-w-3xl lg:flex-row-reverse lg:gap-10'>
-					<div className='text-center lg:text-left'>
-						<img src='/logo.svg' />
-						<h1 className='text-5xl font-bold'>Contact Us</h1>
-						<p className='py-6'>
+			<div className="hero min-h-screen" id="contact">
+				<div className="hero-content py-16 flex-col max-w-3xl lg:flex-row-reverse lg:gap-10">
+					<div className="text-center lg:text-left">
+						<img src="/logo.svg" />
+						<h1 className="text-5xl font-bold">Contact Us</h1>
+						<p className="py-6">
 							We&apos;re eager to hear from you and discuss how we can bring
 							your construction project to life. Whether you have a question,
 							need a quote, or want to explore partnership opportunities, our
@@ -165,5 +164,5 @@ export default function Home() {
 				</div>
 			</div>
 		</main>
-	)
+	);
 }
